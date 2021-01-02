d96f90bdaf9db642eb2055e6c7a37d560d968099
Esteban Romero <esroleo@gmail.com>
2021-01-02 10:02:18 -0500

    function added and assigned to the add tasks button

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,21 +3,21 @@
 
 
 
-var buttonEl = window.document.querySelector("#save-task");
+var buttonEl = window.document.querySelector("#save-task"); // query selector for #save-task id element
 console.log(buttonEl);
 
-var tasksToDoEl = document.querySelector("#tasks-to-do");
+var tasksToDoEl = document.querySelector("#tasks-to-do"); // query selector for #tasks-to-do id element
 
 
 
+var createTaskHandler = function() { // Function to add task which is called by buttonEl onced clicked.
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    listItemEl.textContent = "This is a new task.";
+    tasksToDoEl.appendChild(listItemEl);
+  }
 
-buttonEl.addEventListener("click", function() {
-
-    var taskItemEl = document.createElement("li");
-    taskItemEl.className = "task-item";
-    taskItemEl.textContent = "hello";
-    tasksToDoEl.appendChild(taskItemEl);
-});
+buttonEl.addEventListener("click", createTaskHandler); // Add Task button
 
 
   var counter = 10;
@@ -31,3 +31,5 @@ buttonEl.addEventListener("click", function() {
   }
 
 var startCountDown = setInterval(countdown, 1000);
+
+
