b8aefd85fe582b91789ed1151abcc0fd3c1322fb
Esteban Romero <esroleo@gmail.com>
2021-01-02 09:54:01 -0500

    first task added dynamically

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,23 @@
 //console.log(window.document);
 //console.dir(window.document);
 
+
+
 var buttonEl = window.document.querySelector("#save-task");
 console.log(buttonEl);
 
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+
+
+
 buttonEl.addEventListener("click", function() {
-    alert("button clicked");
-  });
+
+    var taskItemEl = document.createElement("li");
+    taskItemEl.className = "task-item";
+    taskItemEl.textContent = "hello";
+    tasksToDoEl.appendChild(taskItemEl);
+});
 
 
   var counter = 10;
@@ -19,4 +30,4 @@ buttonEl.addEventListener("click", function() {
       }
   }
 
-  var startCountDown = setInterval(countdown, 1000);
+var startCountDown = setInterval(countdown, 1000);
