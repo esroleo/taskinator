7bcba81d5c7afbedd03034e210b967635b6b0992
Esteban Romero <esroleo@gmail.com>
2021-01-02 10:56:50 -0500

    form alignment completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,17 @@
-//console.log(window.document);
-//console.dir(window.document);
+// Taskinator app!
 
-
-
+var formEl = document.querySelector("#task-form"); // Listen to an event happending on the entire form 
 var buttonEl = window.document.querySelector("#save-task"); // query selector for #save-task id element
-console.log(buttonEl);
-
 var tasksToDoEl = document.querySelector("#tasks-to-do"); // query selector for #tasks-to-do id element
 
-
-
-var createTaskHandler = function() { // Function to add task which is called by buttonEl onced clicked.
+var createTaskHandler = function(event) { // Function to add task which is called by buttonEl onced clicked.
+    event.preventDefault();
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
   };
 
-buttonEl.addEventListener("click", createTaskHandler); // Add Task button
+//buttonEl.addEventListener("click", createTaskHandler); // Add Task button
+formEl.addEventListener("submit", createTaskHandler); // Listen to submit on the form.
 
